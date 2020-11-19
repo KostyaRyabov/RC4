de5c4608135674c07d3c7ed257f9b50b3613cf94
Kostya R <45066193+KostyaRyabov@users.noreply.github.com>
2020-11-19 21:43:40 +0300

    Revert "clear"
    This reverts commit aaf46888631e9a77284f0fbeec2c29e3b7d3f0f4.

---
+++ ci/appveyor/install.bat
@@ -0,0 +1,5 @@
+set DOWNLOAD_DIR=c:\downloads
+mkdir %DOWNLOAD_DIR%
+
+del "c:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"
+del "c:\Program Files (x86)\MSBuild\4.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"
